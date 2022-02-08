3bd9c665b45bc98e8442d6e1c481e5137fc7df79
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-02-08 15:19:48 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+import Card from './components/Card';
+import { Container } from './components/styles/Container.styled';
+import GlobalStyles from './components/styles/Global';
+
+import content from './content';
+import Footer from './components/Footer';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#005533',
+  },
+  mobile: '768px',
+};
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => (
+            <Card item={item} />
+          ))}
+        </Container>
+        <Footer />
+      </Fragment>
+    </ThemeProvider>
+  );
+};
+
+export default App;
